c53e3677ae47576c160283bb76b74474779e2f65
Tomi Tomion <6616773+mrdarkside@users.noreply.github.com>
2021-02-28 01:51:05 +0300

    create element function and Didact obj created

--- index.js
+++ index.js
@@ -6,3 +6,45 @@
 // Step VI: Reconciliation
 // Step VII: Function Components
 // Step VIII: Hooks
+
+// Use this comment with "@" before "jsx" to let babel translate jsx
+// using Didact's createElement function
+/** jsx Didact.createElement */
+
+const Didact = {
+  createElement: createElement,
+};
+
+// Spread operator for children so it always be an array
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        //wrap primitives into own object with type TEXT_ELEMENT
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+// React doesn’t wrap primitive values or create empty arrays when there aren’t children
+// just simplification
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
